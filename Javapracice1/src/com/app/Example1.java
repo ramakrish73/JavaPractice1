66a2a68edbf8f6ba84f75b3d0c0807333e495f52
ramakrish73 <ramakrishnan3030@gmail.com>
2026-02-22 13:15:54 +0530

    add

--- Javapracice1/src/com/app/Example1.java
+++ Javapracice1/src/com/app/Example1.java
@@ -5,6 +5,10 @@ public class Example1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		System.out.println("Hello World");
+		
+		System.out.println("Hello World");
+		System.out.println("Hello World");
 
 	}
 
